599cd1bbb73c40e8ebf8e81c28221a18bdf43107
FMisi <felegyimisi@gmail.com>
2021-02-06 22:50:39 +0100

    FMTL

---
+++ Assets/Scripts/LauncherUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class LauncherUI : MonoBehaviour
+{
+    private void Awake(){
+        transform.Find("16by9Btn").GetComponent<Button_UI>().ClickFunc = () => {
+            // Launch!
+            Application.OpenURL("https://www.size43.com/16by9-aspect-ratio-calculator/");
+        };
+    }
+}

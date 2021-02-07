84ab7159586774b588d49ee46d80b3d3b91fe032
FMisi <felegyimisi@gmail.com>
2021-02-07 22:51:07 +0100

    FMTL

--- Assets/Scripts/LauncherUI.cs
+++ Assets/Scripts/LauncherUI.cs
@@ -11,5 +11,17 @@ public class LauncherUI : MonoBehaviour
             // Launch!
             Application.OpenURL("https://www.size43.com/16by9-aspect-ratio-calculator/");
         };
+
+        transform.Find("AIDA64Btn").GetComponent<Button_UI>().ClickFunc = () => {
+            // Launch!
+            string path = Application.dataPath + "/../Builds/AIDA64 Extreme/aida64.exe";
+            Process.Start(path);
+        };
+
+        transform.Find("AudacityBtn").GetComponent<Button_UI>().ClickFunc = () => {
+            // Launch!
+            string path = Application.dataPath + "/../Builds/Audacity/audacity.exe";
+            Process.Start(path);
+        };
     }
 }
